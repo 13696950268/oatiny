4d8debd6b4401880adc1d8c49352d69fe510b9ef
git <18350002327@163.com>
2020-08-05 18:02:35 +0800

    oatiny第2次提交.

--- hrm/src/main/java/com/oatiny/hrm/controller/HrmController.java
+++ hrm/src/main/java/com/oatiny/hrm/controller/HrmController.java
@@ -8,15 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * @description: 人员
+ * @author: zm
+ * @createDate: 2020/8/5 18:00
+ */
 @RestController
 @RequestMapping("/hrm")
 public class HrmController {
     @Autowired
     private HrmUserMapper hrmUserMapper;
 
+    /**
+     * 获取所有人员
+     * @author zm
+     * @param:
+     * @return
+     * @exception
+     * @createDate 2020/8/5 18:01
+     */
     @RequestMapping("/getHrmUserList")
     public List<HrmUser> getHrmUserList(){
         List<HrmUser> hrmUserList = hrmUserMapper.selectList(null);
         return hrmUserList;
     }
+
 }
